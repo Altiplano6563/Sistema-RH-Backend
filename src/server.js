5a671d85a5465851b00c75069365b162a0253be0
Altiplano6563 <vanessagusan@gmail.com>
2025-05-21 16:12:50 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false // Obrigatório no Railway
+        rejectUnauthorized: false
       }
     }
   });
